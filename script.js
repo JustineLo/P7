54f564adcade2562bf179ae5fe0f177a369b47d3
Justine <justine@ecotree.fr>
2022-12-18 14:50:08 +0100

    displayonerecipe function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import recipes from './recipes.js'
+
 async function initFiltersContainers() {
     const ingredientsFilter = document.getElementById('ingredients-filter-container')
 
@@ -18,8 +20,40 @@ async function displayFilterInput(searchContainer) {
     filterInput.value = "wtf"
 }
 
+async function displayOneRecipe(recipe) {
+    const recipeContainer = document.createElement('div');
+    recipeContainer.setAttribute('class', 'recipe-container');
+    recipeContainer.innerHTML = `
+        <div class="recipe-img">
+           
+        </div>
+        <div class="recipe-info">
+            <div class="recipe-header">
+                <h3>${recipe.name}</h3>
+                <div class="recipe-time">
+                    <i class="fa-solid fa-clock"></i>
+                    <p>${recipe.time} min</p>
+                </div>
+            </div>
+            <div class="recipe-body">
+                <div class="recipe-ingredients">
+                    <ul>
+                        ${recipe.ingredients.map(ingredient => `<li>${ingredient.ingredient}${ingredient.quantity ? ": " + ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""} </li>`).join('')}
+
+                    </ul>
+                </div>
+                <div class="recipe-description">
+                    <p>${recipe.description}</p>
+                </div>
+            </div>
+        </div>
+    `
+    document.getElementById('recipes-section').appendChild(recipeContainer)
+}
+
 async function init() {
     initFiltersContainers()
+    displayOneRecipe(recipes[0])
 }
 
 init()
